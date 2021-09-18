8cf055cb766f879ad8119855d6799b2312fddd22
hoichael <31223078+hoichael@users.noreply.github.com>
2021-09-19 00:00:48 +0200

    Basic scrape functionality + format collected data as json

--- 4scrape.js
+++ 4scrape.js
@@ -1,16 +1,20 @@
+#!/usr/bin/env node
+
 // imports
 const puppeteer = require("puppeteer");
 
 // global vars
-const errorText = "oops";
+const errorTextArgs = "oops";
+const errorTextURL = "oops";
 const helpText = "help lol";
+const closeText = "we done";
 
 // entry point
 const init = () => {
   const args = process.argv.slice(2);
 
   if (args.length === 0) {
-    printMessage(errorText);
+    printMessage(errorTextArgs);
   } else if (args.includes("--help") || args.includes("-h")) {
     printMessage(helpText);
   } else {
@@ -26,13 +30,63 @@ const printMessage = (msg) => {
 const initScrape = async (args) => {
   console.log("init");
 
-  const browserInstance = await puppeteer.launch({ headless: false });
+  const browserInstance = await puppeteer.launch({ headless: true });
   const page = await browserInstance.newPage();
 
   try {
     await page.goto(args[0]);
   } catch (error) {
-    printMessage(errorText);
+    printMessage(errorTextURL);
+  }
+
+  try {
+    //    const postContainersArr = await page.$$(".postContainer");
+    //  await manageScrape(args, page);
+  } catch (error) {
+    printMessage(errorTextURL);
+  }
+
+  await manageScrape(args, page);
+
+  printMessage(closeText);
+  await browserInstance.close();
+  process.exit();
+};
+
+const manageScrape = async (args, page) => {
+  const jsonArr = await genJSON(page);
+  console.log(jsonArr);
+};
+
+const genJSON = async (page) => {
+  let arr = [];
+  const containersArr = await page.$$(".postContainer");
+
+  for (let i = 0; i < containersArr.length; i++) {
+    /*
+    const text = await containers[i].$eval(".postMessage", (i) => i.innerText);
+    console.log(text);
+    console.log("\n \n \n");
+*/
+    arr.push({
+      postNum: await containersArr[i].$eval(".postNum", (el) => el.innerText),
+      message: await containersArr[i].$eval(
+        ".postMessage",
+        (el) => el.innerText
+      ),
+      img: await checkForImage(containersArr[i]),
+    });
+  }
+
+  return arr;
+};
+
+const checkForImage = async (element) => {
+  try {
+    const src = await element.$eval("img", (el) => el.getAttribute("src"));
+    return src;
+  } catch (error) {
+    return undefined;
   }
 };
 
